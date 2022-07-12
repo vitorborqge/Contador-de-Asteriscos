3e03f9a97ae917f0c91203dcd8868cd9f394812c
Vitor Borges Vieira <101013550+vitorborqge@users.noreply.github.com>
2022-07-12 15:40:38 -0300

    Add files via upload

---
+++ Contador de Asteriscos.js
@@ -0,0 +1,23 @@
+// Criar uma função que exibe a quantidade de * que aquela linha possui
+
+exibirAsteriscos(10);
+
+function exibirAsteriscos(linhas) {
+         let padrao = '';
+         for(let linha = 1; linha <= linhas; linha++){
+                  padrao += '*';
+                  console.log(padrao);
+         }
+}
+
+// outra forma
+
+function exibirAsteriscos(linhas) {
+          for(let linha = 1; linha <= linhas; linha++){
+                   let padrao = '';
+                   for(let i = 0; i < linha; i++){
+                         padrao += '*';
+              }
+              console.log(padrao);
+       }
+}
